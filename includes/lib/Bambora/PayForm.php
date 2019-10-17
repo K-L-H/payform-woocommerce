5e42d71f87aa3d80376da3629b024fca42f9b7af
Markus Salminen <markus.salminen@paybyway.com>
2019-10-17 10:24:05 +0300

    Bumps version to 2.1.7

--- includes/lib/Bambora/PayForm.php
+++ includes/lib/Bambora/PayForm.php
@@ -66,7 +66,7 @@ class PayForm
 		else
 			$payment_data['plugin_info'] .= '0';
 
-		$payment_data['plugin_info'] .= '|2.1.6';
+		$payment_data['plugin_info'] .= '|2.1.7';
 
 		return $this->makeRequest($url, $payment_data);
 	}
